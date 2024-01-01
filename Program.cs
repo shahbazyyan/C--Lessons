2ce87bd29dea6842459c07986f6673f01dcd278b
shahbazyyan <shahbazyan.daviit@gmail.com>
2024-01-01 12:54:33 +0400

    Learned for, while and do while loops

--- Program.cs
+++ Program.cs
@@ -3,23 +3,48 @@
 namespace mycs_project
 {
 
-	class Program
-	{
-		static void Main()
-		{
-			short user_input = Convert.ToInt16(Console.ReadLine());
+ class Program
+ {
+  static void Main()
+  {
 
-			switch (user_input)
-			{
-				case 1: System.Console.WriteLine("Number is 1"); break;
-				case 2: System.Console.WriteLine("Number is 2"); break;
-				case 3: System.Console.WriteLine("Number is 3"); break;
-				case 4: System.Console.WriteLine("Number is 4"); break;
-				default:
-					Console.WriteLine("Number not found!");
-					break;
-			}
-		}
-	}
+   // for (byte i = 0; i <= 21; i++) {
+   //  Console.WriteLine("Element: {0}", i);
+   // }
+
+   // for (float i = 200; i > 10; i/=2) {
+   //   Console.WriteLine("Element: {0}", i);
+   // }
+
+   // int i = 0;
+   // while (i <= 15)
+   // {
+   //  Console.WriteLine("Element: {0}", i);
+   //  i++;
+   // }
+
+   // bool isAlive = true;
+   // while(isAlive) {
+   //  string end = Console.ReadLine();
+   //  if(end == "end") 
+   //        isAlive = false;
+   // }
+
+
+   // byte i = 100;
+
+   //  do {
+   //     Console.WriteLine("Element: " + i);
+   //  } while (i < 10);
+
+
+   for (short i = 0; i < 100; i++) {
+    // if(i > 5) break;
+    if(i % 2 == 0) continue;
+    Console.WriteLine("Element: " + i);
+   }
+
+  }
+ }
 
 }
