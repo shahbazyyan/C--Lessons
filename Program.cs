6d3ad6c217d621314a3261912460c602eb29f1d2
shahbazyyan <shahbazyan.daviit@gmail.com>
2024-01-01 17:15:42 +0400

    Learned foreach loop and some array methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace mycs_project
 {
@@ -8,56 +9,39 @@ namespace mycs_project
   static void Main()
   {
 
-   // int[] numbers = new int[5];
-   // numbers[0] = 20;
-   // numbers[1] = 50;
-   // numbers[2] = 100;
-   // numbers[3] = 150;
-   // numbers[4] = 210;
+   // short[] nums = {1,2,4,5,6,7};
 
-   // System.Console.WriteLine("El: " + numbers[2]); 
-
-   // string[] words = new string[] {"John", "Alex", "Snow"};
-
-   // words[1] = "Bob";
-
-   // for (int i = 0; i < numbers.Length; i++) {
-   //  Console.WriteLine("El: " + numbers[i] + 1);
+   // foreach(short item in nums) {
+   //  Console.WriteLine("Item: " + item);
    // }
 
+   // short[,] numbers = {
+   //  {2,3,4},
+   //  {24,22,43},
+   //  {6,77,34},
+   // };
 
-   // short[] numbers = new short[10];
-   // Random random = new Random();
-   // short val = 0;
-
-   // for (byte i = 0; i < numbers.Length; i++)
-   // {
-   //  numbers[i] = Convert.ToInt16(random.Next(11, 97));
-   //  Console.WriteLine("Element: " + numbers[i]);
-
-   //  val += numbers[i];
+   // foreach(short item in numbers) {
+   //  Console.WriteLine("Item: " + item);
    // }
-   // Console.WriteLine("Value: " + val);
 
-   char[,] symbols = new char[2, 2];
-   symbols[0, 0] = 'k';
+   List<int> nums = new List<int> () {
+    5,6,8
+   };
 
+   nums.Add(222);
+   nums.Add(123);
 
+   nums.Remove(222);
+   nums.Sort();
+   nums.Reverse();
+   // nums.Clear();
+   bool containsEight = nums.Exists(item => item == 8);
+   Console.WriteLine(containsEight);
+   
 
-   int[,] nums = {
-    {5, 6, 7},
-    {1, 3, 2},
-    {3, 3, 3}
-};
-
-   nums[1, 2] = 5;
-
-   for (int i = 0; i < nums.GetLength(0); i++) // Loop through rows
-   {
-    for (int j = 0; j < nums.GetLength(1); j++) // Loop through columns
-    {
-     Console.WriteLine(nums[i, j]);
-    }
+   foreach(int item in nums) {
+    Console.WriteLine(item);
    }
 
 
