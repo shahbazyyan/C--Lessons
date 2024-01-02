2e8bb265eda430ecfab7574717ed040d87eeea11
shahbazyyan <shahbazyan.daviit@gmail.com>
2024-01-02 18:18:07 +0400

    Learned try catch finally operators

--- Program.cs
+++ Program.cs
@@ -8,39 +8,37 @@ namespace mycs_project
     {
         static void Main()
         {
-            // Console.WriteLine("Type something: ");
-            // string text = Console.ReadLine();
-            // using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
-            // {
-            //  byte[] ary = System.Text.Encoding.Default.GetBytes(text);
 
-            //  stream.Write(ary, 0, ary.Length);
+            // try
+            // {
+            //     int num = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine(num);
+            // }
+            // catch (FormatException)
+            // {
+            //     Console.WriteLine("Something went wrong!");
             // }
 
-            string txt = "Hello dears!";
-            using (FileStream stream = new FileStream("text.txt", FileMode.OpenOrCreate))
+            try
             {
-                byte[] arrtxt = System.Text.Encoding.Default.GetBytes(txt);
-                stream.Write(arrtxt, 0, txt.Length);
+                int a = 5, b = 0, res;
+                Console.WriteLine("Type number");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Type second number");
+                b = Convert.ToInt32(Console.ReadLine());
+                res = a / b;
+                Console.WriteLine("Res: " + res);
             }
-
-            //   using(FileStream stream1 = File.OpenRead("info.txt")) {
-            //    byte[] arr = new byte[stream1.Length];
-            //    stream1.Read(arr, 0, arr.Length);
-
-            //    string textFromFile = System.Text.Encoding.Default.GetString(arr);
-            //    Console.WriteLine(textFromFile);
-            //   }
-
-            using (FileStream stream = File.OpenRead("text.txt"))
+            catch (DivideByZeroException)
             {
-                byte[] newarr = new byte[stream.Length];
-                stream.Read(newarr, 0, newarr.Length);
-                string textFromFile = System.Text.Encoding.Default.GetString(newarr);
-                Console.WriteLine(textFromFile);
-
-       
-     }
+                Console.WriteLine("Something went wrong!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Something went wrong!");
+            } finally {
+                Console.WriteLine("Finaaaly!");
+            }
 
         }
     }
