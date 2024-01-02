cb15e0e6ebd08ff70a35341e5996feca4027c38f
shahbazyyan <shahbazyan.daviit@gmail.com>
2024-01-02 11:36:22 +0400

    Learned work with files, open or create, read or create, etc...

--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
 ﻿using System;
+using System.IO;
 
 namespace mycs_project
 {
 
- class Program
- {
-  static void Main()
-  {
-    string word = "hello";
-    Console.WriteLine(word.Substring(0, word.Length - 1));
-    // Console.WriteLine(word.Trim());
-    // word = string.Concat(word, "!!");
-    // Console.WriteLine(String.Compare(word, "hello"));
+    class Program
+    {
+        static void Main()
+        {
+            // Console.WriteLine("Type something: ");
+            // string text = Console.ReadLine();
+            // using (FileStream stream = new FileStream("info.txt", FileMode.OpenOrCreate))
+            // {
+            //  byte[] ary = System.Text.Encoding.Default.GetBytes(text);
 
-   // string people = "Alex,Bob,Jhon";
-   // string[] names = people.Split(new char[]{','});
+            //  stream.Write(ary, 0, ary.Length);
+            // }
 
-   // people = String.Join("-", names);
-   // Console.WriteLine(people);
+            string txt = "Hello dears!";
+            using (FileStream stream = new FileStream("text.txt", FileMode.OpenOrCreate))
+            {
+                byte[] arrtxt = System.Text.Encoding.Default.GetBytes(txt);
+                stream.Write(arrtxt, 0, txt.Length);
+            }
 
-   // foreach (string item in names)
-   // {
-   //  Console.WriteLine(item);
-   // }
+            //   using(FileStream stream1 = File.OpenRead("info.txt")) {
+            //    byte[] arr = new byte[stream1.Length];
+            //    stream1.Read(arr, 0, arr.Length);
 
-  }
- }
+            //    string textFromFile = System.Text.Encoding.Default.GetString(arr);
+            //    Console.WriteLine(textFromFile);
+            //   }
+
+            using (FileStream stream = File.OpenRead("text.txt"))
+            {
+                byte[] newarr = new byte[stream.Length];
+                stream.Read(newarr, 0, newarr.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(newarr);
+                Console.WriteLine(textFromFile);
+
+       
+     }
+
+        }
+    }
 }
