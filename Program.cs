369512dd744952be2a9afc5dbf8d893325b285cd
shahbazyyan <shahbazyan.daviit@gmail.com>
2024-01-08 20:29:28 +0400

    Learned overloads

--- Program.cs
+++ Program.cs
@@ -10,15 +10,34 @@ namespace mycs_project
   static void Main()
   {
 
-   Bot bot = new Bot("Bot", 96, new byte[] { 3, 5, 7 });
-   bot.printValues();
+   //    Bot bot = new Bot("Bot", 96, new byte[] { 3, 5, 7 });
+   //    bot.printValues();
 
-   Killer killer = new Killer("Killer", 980, new byte[] { 4, 6, 9 }, 100);
-   killer.printValues();
-   killer.Lazer();
+   //    Killer killer = new Killer("Killer", 980, new byte[] { 4, 6, 9 }, 100);
+   //    killer.printValues();
+   //    killer.Lazer();
 
-   Bot bot1 = new Bot();
+      Bot bot1 = new Bot();
+      bot1.setValues("Mike");
+      
+
+   // Multiply(7, 89);
+   // Multiply(7.5f, 89.7f);
+
 
   }
+
+  public static void Multiply(int a, int b)
+  {
+   int res = a * b;
+   Console.WriteLine("Result: " + res);
+  }
+
+  public static void Multiply(float a, float b)
+  {
+   float res = a * b;
+   Console.WriteLine("Result: " + res);
+  }
+
  }
 }
--- Robot.cs
+++ Robot.cs
@@ -3,95 +3,100 @@ using System;
 namespace mycs_project
 {
 
-  abstract class Robot : IRun, IJump
+ abstract class Robot : IRun, IJump
+ {
+
+  private static int conut;
+  private string? name;
+  private int weight;
+  private byte[]? cordinates;
+  protected string? rank;
+
+  public byte[]? Cordinates
   {
-
-    private static int conut;
-    private string? name;
-    private int weight;
-    private byte[]? cordinates;
-    protected string? rank;
-
-    public byte[]? Cordinates
-    {
-      get
-      {
-        return cordinates;
-      }
-      private set { }
-    }
-    public string? Name
-    {
-      get
-      {
-        return name;
-      }
-      private set { }
-    }
-
-    public int Weight
-    {
-      get
-      {
-        return this.weight;
-      }
-      set
-      {
-        if (value < 1)
-          this.weight = 1;
-        else if (value > 5000)
-          this.weight = 5000;
-        else
-          this.weight = value;
-      }
-    }
-
-    public int Width { private get; set; }
-    public float speed { get; set; }
-    public float y { get; set; }
-
-    public Robot(string _name, int _weight, byte[] _cordinates)
-    {
-      Console.WriteLine("Object has been created");
-      setValues(_name, _weight, _cordinates);
-      conut++;
-    }
-
-    public Robot()
-    {
-      conut++;
-    }
-
-    public Robot(string name)
-    {
-      Console.WriteLine("Object has been created");
-      this.name = name;
-      conut++;
-    }
-
-
-    public void setValues(string name, int weight, byte[] cordinates)
-    {
-      this.name = name;
-      this.weight = weight;
-      this.cordinates = cordinates;
-    }
-
-    public abstract void printValues();
-
-    public static void Print()
-    {
-      Console.WriteLine("Conut: " + conut);
-    }
-
-    public void RobotRun()
-    {
-      Console.WriteLine("Running Robot");
-    }
-
-    public void RobotJump()
-    {
-      Console.WriteLine("Running Jumping");
-    }
+   get
+   {
+    return cordinates;
+   }
+   private set { }
   }
+  public string? Name
+  {
+   get
+   {
+    return name;
+   }
+   private set { }
+  }
+
+  public int Weight
+  {
+   get
+   {
+    return this.weight;
+   }
+   set
+   {
+    if (value < 1)
+     this.weight = 1;
+    else if (value > 5000)
+     this.weight = 5000;
+    else
+     this.weight = value;
+   }
+  }
+
+  public int Width { private get; set; }
+  public float speed { get; set; }
+  public float y { get; set; }
+
+  public Robot(string _name, int _weight, byte[] _cordinates)
+  {
+   Console.WriteLine("Object has been created");
+   setValues(_name, _weight, _cordinates);
+   conut++;
+  }
+
+  public Robot()
+  {
+   conut++;
+  }
+
+  public Robot(string name)
+  {
+   Console.WriteLine("Object has been created");
+   this.name = name;
+   conut++;
+  }
+
+
+  public void setValues(string name, int weight, byte[] cordinates)
+  {
+   this.name = name;
+   this.weight = weight;
+   this.cordinates = cordinates;
+  }
+
+  public void setValues(string name)
+  {
+   this.name = name;
+  }
+
+  public abstract void printValues();
+
+  public static void Print()
+  {
+   Console.WriteLine("Conut: " + conut);
+  }
+
+  public void RobotRun()
+  {
+   Console.WriteLine("Running Robot");
+  }
+
+  public void RobotJump()
+  {
+   Console.WriteLine("Running Jumping");
+  }
+ }
 }
